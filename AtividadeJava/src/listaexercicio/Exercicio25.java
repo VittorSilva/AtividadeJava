60bab40633d091e7301b0feebc93b3ae957cf8fc
Vitor Silva <vsr072001@icloud.com>
2022-12-16 01:04:38 -0300

    Lista de exercicios completa

---
+++ AtividadeJava/src/listaexercicio/Exercicio25.java
@@ -0,0 +1,27 @@
+package listaexercicio;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio25 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira um número");
+        int numero = sc.nextInt();
+        if(numero > 0) {
+            for (int i = 1; i >= 0; i++) {
+                System.out.print("Insira mais um " + i + "º número: ");
+                numero += i;
+                System.out.println("A soma é: "+ numero);
+            }
+        }else {
+            System.out.println("Foi inserido um número negativo");
+        }
+
+	}
+
+}
